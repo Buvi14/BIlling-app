b04069c7688e49fc501284c707477e9b12bbf5fa
Buvi14 <40881963+Buvi14@users.noreply.github.com>
2018-07-08 00:04:41 +0530

    Add files via upload

--- billing1.py
+++ billing1.py
@@ -20,9 +20,31 @@ class BaseSubscriptionView(BaseBillingDetailsView):
         product = billing.loading.get_product(self.kwargs['product'])
         context['product'] = product
         return context
-    def form_valid(self, form):
-        response = super(BaseSubscriptionView, self).form_valid(form)
-
-        ba = self.request.user.billing_account            # create subscription
-        ba.subscribe_to_product(self.kwargs['product'])   # return the redirect
-        return response
+class BillingDetailsView(BaseBillingDetailsView):         #show the billing detials
+    template_name = 'billing/details.html'
+    def get_form_class(self):
+        billing_account = self.request.user.billing_account
+        processor = billing_account.get_processor()
+        return processor.get_billing_details_form(billing_account)
+class BillingHistoryView(TemplateView):                  #main function
+    template_name = 'billing/history.html'
+    pass
+    def dispatch(request, *args, **kwargs):
+        cur_product_cls = request.user.billing_account.get_current_product_class()
+        req_product_name = kwargs['product']
+        try:
+            req_product_cls = billing.loading.get_product(req_product_name)
+        except ValueError:
+            raise Http404
+        if req_product_cls not in request.user.billing_account.get_visible_products():
+            raise Http404
+        if (req_product_cls.get_requires_payment_details()and not request.user.billing_account.has_valid_billing_details()):
+            return billing_details_view(request, *args, **kwargs)
+        elif (
+            not req_product_cls.get_requires_payment_details()
+            or request.user.billing_account.has_valid_billing_details()
+        ):
+            return confirmation_view(request, *args, **kwargs)
+        else:
+            raise RuntimeError('Error: null condition should never occur')
+     return dispatch
